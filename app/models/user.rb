76809cdae69b859aa7a8309837c8a223d08da0be
mlemansk <mlemansk@chicagobooth.edu>
2021-04-17 04:38:44 +0000

    adding

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :comments, inverse_of: 'author'
+  has_many :comments, foreign_key: :author_id #inverse_of: 'author'
   has_many :photos, inverse_of: 'owner'
   has_many :follow_requests, inverse_of: 'recipient'
   has_many :follow_requests, inverse_of: 'sender'
