8c5af61f2cdd0ff723a37646690268ea79349c77
mlemansk <mlemansk@chicagobooth.edu>
2021-04-18 23:09:35 +0000

    added

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -23,4 +23,6 @@ class FollowRequest < ApplicationRecord
   enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" }
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
+
+  
 end
