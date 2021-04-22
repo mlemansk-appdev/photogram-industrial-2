0d3c57fc0a99e83adbb428dace96cdf3b42d9f69
mlemansk <mlemansk@chicagobooth.edu>
2021-04-22 00:26:36 +0000

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get ":username/liked" => "photos#liked", as: :liked_photos
   get ":username/feed" => "photos#feed", as: :feed_photos
   get ":username/followers" => "users#followers", as: :followers
-  get ":username/leaders" => "users#leaders", as: :leaders 
+  get ":username/following" => "users#leaders", as: :leaders 
   
   get ":username" => "users#show", as: :user # This always has to be the last route since it is so general
 end
